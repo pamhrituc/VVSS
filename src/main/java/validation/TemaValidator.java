d1291eb8178a074ca5430ba0ddc16e9d7cf28219
Pamela Ottilia Hrituc <44650892+pamhrituc@users.noreply.github.com>
2020-04-07 17:30:33 +0300

    Bug fix

--- src/main/java/validation/TemaValidator.java
+++ src/main/java/validation/TemaValidator.java
@@ -3,16 +3,20 @@ import domain.Tema;
 
 public class TemaValidator implements Validator<Tema> {
     public void validate(Tema tema) throws ValidationException {
+//        System.out.println(tema.getID());
+//        System.out.println(tema.getDescriere());
+//        System.out.println(tema.getDeadline());
+//        System.out.println(tema.getStartline());
         if (tema.getID() == null || tema.getID().equals("")) {
             throw new ValidationException("ID invalid! \n");
         }
         if (tema.getDescriere() == null || tema.getDescriere().equals("")) {
             throw new ValidationException("Descriere invalida! \n");
         }
-        if (tema.getDeadline() < 1 || tema.getDeadline() > 14 || tema.getDeadline() > tema.getStartline()) {
+        if (tema.getDeadline() < 1 || tema.getDeadline() > 14 || tema.getDeadline() < tema.getStartline()) {
             throw new ValidationException("Deadline invalid! \n");
         }
-        if (tema.getStartline() < 1 || tema.getStartline() > 14 || tema.getStartline() < tema.getDeadline()) {
+        if (tema.getStartline() < 1 || tema.getStartline() > 14 || tema.getStartline() > tema.getDeadline()) {
             throw new ValidationException("Data de primire invalida! \n");
         }
     }
